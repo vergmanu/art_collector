b080f40a406a4a7c6e0d0b3b70be1b202f4b7153
vergmanu <vergmanu@gmail.com>
2022-04-10 20:47:46 -0700

    part one

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
+
+
+class Art:  
+  def __init__(self, title, artist, style, year):
+    self.title = title
+    self.artist = artist
+    self.style = style
+    self.year = year
+
+art = [
+  Art('Sunflowers', 'Van Gogh', 'impressionist', 1888),
+  Art('The Dance Class', 'Edgar Degas', 'impressionist', 1870),
+  Art('Water Lilies', 'Claude Monet', 'impressionist', 1895),
+]
+
+
+
 # Create your views here.
+def home(request):
+    return HttpResponse('<h1>Hello</h1>')
+
+def about(request):
+    return render(request, 'about.html')
+
+def art_index(request):
+    return render(request, 'art/index.html', { 'art': art })
